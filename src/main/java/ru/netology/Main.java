d159272c77af47826315aeddabecf05792b310f8
Nika_Miku <diademistv@gmail.com>
2023-08-01 00:50:00 +0300

    add XML-JSON-Parser

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -7,20 +7,34 @@ import com.opencsv.*;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "csv/data.csv";
-        List<Employee> list = parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
-        writeString(json, "csv/data.json");
+        List<Employee> csvList = parseCSV(columnMapping, fileName);
+        String csvToJson = listToJson(csvList);
+        writeString(csvToJson, "csv/data.json");
+        //XML -> JSON Parser
+        List<Employee> xmlList = parseXML();
+        String xmlToJson = listToJson(xmlList);
+        writeString(xmlToJson, "xml/data2.json");
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -52,5 +66,33 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<Employee> parseXML() {
+        List<Employee> employees = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse("xml/data.xml");
+            doc.getDocumentElement().normalize();
+            NodeList nodeList = doc.getElementsByTagName("employee");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                int id = Integer.parseInt(element.getElementsByTagName("id").item(0).getTextContent());
+                String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                String country = element.getElementsByTagName("country").item(0).getTextContent();
+                int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+
+                Employee employee = new Employee(id, firstName, lastName, country, age);
+                employees.add(employee);
+            }
+        }
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            e.printStackTrace();
+        }
+        return employees;
     }
 }
